111e2398996a86f01ca1bd46d2c7011523c61911
quocvietit <vuongquocviet1996@gmail.com>
2019-05-12 08:08:01 +0700

    Edit

--- web/sh-service/controllers/device_controller.py
+++ web/sh-service/controllers/device_controller.py
@@ -7,7 +7,7 @@
 ==============================================================
 """
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from extenstion.mqtt_core import mqtt
 from setting import MQTTConfiguration
 
@@ -17,12 +17,16 @@ device_controller = Blueprint(__name__, __name__, template_folder='templates')
 @device_controller.route('/device/flash-light/control/on', methods=['GET'])
 def turn_on_light():
     mqtt.publish(MQTTConfiguration.TOPIC_FLASH_LIGHT_CONTROL, "1")
-    return "Ok"
+    mqtt.publish(MQTTConfiguration.TOPIC_FLASH_LIGHT_CONTROL, "1")
+    mqtt.publish(MQTTConfiguration.TOPIC_FLASH_LIGHT_CONTROL, "1")
+    return jsonify({"res":"ok"})
 
 
 @device_controller.route('/device/flash-light/control/off', methods=['GET'])
 def turn_off_light():
     mqtt.publish(MQTTConfiguration.TOPIC_FLASH_LIGHT_CONTROL, "0")
-    return "Ok"
+    mqtt.publish(MQTTConfiguration.TOPIC_FLASH_LIGHT_CONTROL, "0")
+    mqtt.publish(MQTTConfiguration.TOPIC_FLASH_LIGHT_CONTROL, "0")
+    return jsonify({"res":"ok"})
 
 
